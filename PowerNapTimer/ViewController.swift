bb90bd6c8f81d48eb1e4e381334610aefb62cd53
brittonbaird <brittonb000@gmail.com>
2017-07-25 12:33:29 -0600

    Implement Local Notifications

--- PowerNapTimer/ViewController.swift
+++ PowerNapTimer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
     
@@ -14,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     
     let myTimer = MyTimer()
+    let userNotificationIdentifier = "timerNotifiction"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,8 +42,10 @@ class ViewController: UIViewController {
     @IBAction func startButtonTapped(_ sender: Any) {
         if myTimer.isOn {
             myTimer.stopTimer()
+            cancelLocalNotification()
         } else {
             myTimer.startTimer(3)
+            scheduleLocalNotifications()
         }
         setView()
     }
@@ -50,17 +54,57 @@ class ViewController: UIViewController {
     
     func setupAlertController() {
         let alert = UIAlertController(title: "Wake up!", message: "Get up ya lazy bum!", preferredStyle: .alert)
-        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Sleep a few more minutes..."
+            textField.keyboardType = .numberPad
+        }
         
         let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel) { (_) in
             self.setView()
         }
         
+        let snoozeAction = UIAlertAction(title: "Snooze", style: .default) { (_) in
+            guard let timeText = alert.textFields?.first?.text,
+                let time = TimeInterval(timeText) else { return }
+            self.myTimer.startTimer(time)
+            self.scheduleLocalNotifications()
+            self.setView()
+        }
+        
         alert.addAction(dismissAction)
+        alert.addAction(snoozeAction)
         
         present(alert, animated: true, completion: nil)
     }
     
+    // MARK: - UserNotifications
+    
+    func scheduleLocalNotifications() {
+        // Step 1: Create the notification you want to display
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = "Wake Up!"
+        notificationContent.body = "Time to get up."
+        
+        // Step 2: Create a trigger of when you want to display the notification
+        guard let timeRemaining = myTimer.timeRemaining else { return }
+        let fireDate = Date(timeInterval: timeRemaining, since: Date())
+        let dateComponents = Calendar.current.dateComponents([.minute, .second], from: fireDate)
+        let dateTrigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // Step 3: Create the request to send to NotificationCenter
+        let request = UNNotificationRequest(identifier: userNotificationIdentifier, content: notificationContent, trigger: dateTrigger)
+        
+        // Step 4: Adding our request to NotificationCenter
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("Unable to add notification request. \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func cancelLocalNotification() {
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [userNotificationIdentifier])
+    }
 }
 
 // MARK: - TimerDelegate 
@@ -79,7 +123,7 @@ extension ViewController: TimerDelegate {
     
     func timerStopped() {
         setView()
-        // Cancel notification
+        cancelLocalNotification()
     }
 }
 
